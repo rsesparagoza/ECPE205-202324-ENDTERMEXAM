52f11bebf75c2d5b35d29b751003fb5135927855
rsesparagoza <111894959+rsesparagoza@users.noreply.github.com>
2024-05-18 17:35:41 +0800

    CoursePanel initial commit

--- src/CoursePanel.java
+++ src/CoursePanel.java
@@ -14,5 +14,40 @@ public class CoursePanel extends JPanel {
     private void init(){
         courseTableModel = new CourseTableModel();
         courseTable = new JTable(courseTableModel);
+
+	courseNameLabel = new JLabel("Course Name: ");
+	courseCodeLabel = new JLabel("Course Code: ");
+	courseNameField = new JTextField(15);
+	courseCodeField = new JTextField(15);
+	addBtn = new JButton("ADD");
+
+	setLayout(new BorderLayout());
+
+	JPanel coursePanel = new JPanel();
+	coursePanel.setLayout(new GridBagLayout(5,5));
+	coursePanel.add(courseNameLabel);
+	coursePanel.add(courseCodeLabel);
+	coursePanel.add(courseCodeField);
+	coursePanel.add(courseNameField);
+	coursePanel.add(addBtn);
+
+	add(coursePanel, BorderLayout.NORTH)
+	add(new JScrollPane(courseTable, BorderLayout.CENTER);
+
     }
+        addBtn.addActionListener(new ActionListener(){
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String code = courseCodeField.getText();
+            String name = courseNameField.getText();
+
+            if(!code.isEmpty() && !name.isEmpty()) {
+                courseTableModel.addCourse(new Course(courseCode, name));
+                courseCodeField.setText("");
+                courseNameField.setText("");
+            } else {
+                JOptionPane.showMessageDialog(null, "No field should be empty.");
+            }
+        }
+    });
 }
